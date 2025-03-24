5de559e3d447c853b9862b50108294b381dccb9e
Shyan Vilvarajah <100378025+ShyanVilva@users.noreply.github.com>
2025-03-23 20:49:27 -0400

    Project Setup

---
+++ FileSharing.java
@@ -0,0 +1,23 @@
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+// Function to discovery peers on local network
+
+// public class FileSharing {
+//     public static void main(String[] args) {
+//         try {
+//             JmDNS jmdns = JmDNS.create();
+//             ServiceInfo[] serviceInfos = jmdns.list("_http._tcp.local.");
+//             for (ServiceInfo serviceInfo : serviceInfos) {
+//                 System.out.println(serviceInfo.getName());
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+
+public static void helloWorld() {
+    System.out.println("Hello World!");
+}
+    public static void main(String[] args) {
+helloWorld();
+    }
+
